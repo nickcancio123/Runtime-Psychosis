e3942cecdc63e11ced6b7bee528197fcd1fc48fc
nickcancio <nickcancio123@gmail.com>
2021-10-03 04:45:40 -0400

    Finished guard AI

--- Assets/Scripts/Player/Movement/MovementController.cs
+++ Assets/Scripts/Player/Movement/MovementController.cs
@@ -7,12 +7,23 @@ public class MovementController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GroundContact groundContact;
-
+    public Animator animator;
+    
     private bool canMove = true;
     public bool CanMove() => canMove;
+    public void DisableMovement() => canMove = false;
 
     private List<MovementModifier> modifiers = new List<MovementModifier>();
     public void AddModifier(MovementModifier modifier) => modifiers.Add(modifier);
+    
+    private int faceDirection;
+    private Vector3 initialScale;
+
+    private void Start()
+    {
+        canMove = true;
+        initialScale = transform.localScale;
+    }
 
     private void Update()
     {
@@ -21,5 +32,14 @@ public class MovementController : MonoBehaviour
             foreach(MovementModifier mod in modifiers)
                 mod.Move();
         }
+        
+        SetFaceDirection();
     }
+
+    void SetFaceDirection()
+    {
+        faceDirection = (int)Mathf.Sign(rb.velocity.x);
+        transform.localScale = new Vector3(initialScale.x * faceDirection, initialScale.y, initialScale.z);
+    }
+    
 }
